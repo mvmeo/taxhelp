c54b2199b0832567d36fe82fb0f7d609453425bf
Juan Pablo Schiaffino <jpschiaffino@gmail.com>
2022-04-03 12:40:06 -0400

    <html></html>

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tax Help</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <style type="text/css">
+            body{
+                background-color: #fdc;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6">
+                    <form method="POST">
+                        <label>Ingresa el honorario bruto de enero</label>
+                    
+                    </form>
+                </div>
+                <div class="col-lg-3"></div>
+            </div>
+        </div>
+        
+    </body>
+</html>
