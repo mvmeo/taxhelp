c23439d758cff36415b2e4581f609b103d3a757b
Juan Pablo Schiaffino <jpschiaffino@gmail.com>
2022-04-03 19:21:28 -0400

    form + options with the calculations

--- index.php
+++ index.php
@@ -16,7 +16,23 @@
                 <div class="col-lg-6">
                     <form method="POST">
                         <label>Ingresa el honorario bruto de enero</label>
-                    
+                        <input type="number" name="enero" placeholder="Honorario Bruto Enero" class="form-control">
+                        <br>
+                        <label>Ingresa el honorario bruto de febrero</label>
+                        <input type="number" name="febrero" placeholder="Honorario Bruto Febrero" class="form-control">
+                        <br>
+                        <label>Ingresa el honorario bruto de marzo</label>
+                        <input type="number" name="marzo" placeholder="Honorario Bruto Marzo" class="form-control">
+                        <br>
+                        <p>Escoge alguna operación del servicio de impuestos internos: </p>
+                        <select name="Operate" class="form-control">
+                            <option>Ninguna</option>
+                            <option>Sumatoria Impuestos Retenidos</option>
+                            <option>Sumatoria Honorarios Brutos Retenidos</option>
+                            <option>Deuda o Pago en Abril</option>
+                        </select>
+                        <input type="submit" name="submit" value="Calculate" class="btn btn-sucess">
+                        <input type="reset" value="Clear" class="btn btn-danger">
                     </form>
                 </div>
                 <div class="col-lg-3"></div>
